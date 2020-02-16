9b730f8ee3a3d8674a1bd6f90fd843b7c48f2ab5
dmitryalexeevi4 <dmitryalexeevi4@yandex.ru>
2020-02-16 19:16:07 +0300

    Домашняя работа №1 (Прогрессия для N чисел)

--- src/part1/lesson01/task04/Solution.java
+++ src/part1/lesson01/task04/Solution.java
@@ -1,21 +1,39 @@
 package part1.lesson01.task04;
+
 import java.util.Scanner;
+
 //Арифметическая и геометрическая прогрессия
 public class Solution {
+
+    public static int n = 10;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите первое число: ");
         int i = scanner.nextInt();
-        System.out.println("Введите второе: ");
+        System.out.println("Введите второе число: ");
         int j = scanner.nextInt();
         arithmProg(i, j);
         geomProg(i, j);
     }
-    public static void arithmProg(int a, int d){
-        System.out.println("Арифметическая прогрессия: " + (a + " " + (a + d) + " " + (a + d + d) + " " + (a + d + d + d)) + "...");
-    }
-    public static void geomProg(int b, int q){
-        System.out.println("Геометрическая прогрессия: " + (b + " " + (b * q) + " " + (b * q * q) + " " + (b * q * q * q)) + "...");
+
+    public static void arithmProg(int firstDigit, int secondDigit) {
+        int scannedDigit = firstDigit;
+        System.out.print("Арифметическая прогрессия: " + scannedDigit + " ");
+        for (firstDigit = scannedDigit; firstDigit < n; firstDigit++) {
+            System.out.print((firstDigit + secondDigit) + " ");
+            secondDigit++;
+        }
+        System.out.println();
     }
 
+    public static void geomProg(int firstDigit, int secondDigit) {
+        int thirdDigit = secondDigit;
+        System.out.print("Геометрическая прогрессия: " + firstDigit + " " + secondDigit + " ");
+        for (int i = thirdDigit; i < n; i++) {
+            System.out.print(((firstDigit * secondDigit) * thirdDigit) + " ");
+            thirdDigit *= secondDigit;
+        }
+        System.out.println();
+    }
 }
