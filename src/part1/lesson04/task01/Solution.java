4275b566681e85123e546942cbc0f81113b43a9f
dmitryalexeevi4 <dmitryalexeevi4@yandex.ru>
2020-02-17 22:53:00 +0300

    Домашняя работа №4 (task01 final)

--- src/part1/lesson04/task01/Solution.java
+++ src/part1/lesson04/task01/Solution.java
@@ -1,28 +1,19 @@
 package part1.lesson04.task01;
 
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 //Программа с методом, заменяющим все вхождения слова "бяка"
 class Solution {
     public static void main(String[] args) {
         System.out.println("Введите строку: ");
         Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
 
-        wordReplace(s);
+        wordReplace(scanner.nextLine());
     }
 
     private static void wordReplace(String word) {
-        String badWord = "бяка";
-        Pattern pattern = Pattern.compile(badWord);
-        Matcher matcher = pattern.matcher(word);
+        String badWord = "бяк[а-яА-я]", censored = "\"вырезано цензурой\"";
+        System.out.println(word.replaceAll(badWord, censored));
 
-        if (!matcher.find()) {
-            System.out.println(word);
-        } else {
-            System.out.println(word.replace(badWord, "\"вырезано цензурой\""));
-        }
     }
 }
